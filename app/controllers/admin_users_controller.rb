d345f8db378811bf7e92d769079df75cc0afc3c6
Guillaume Fafard <guillaume@next-media.org>
2012-03-01 16:39:13 -0500

    Signed-off-by: Guillaume Fafard <guillaume@next-media.org>

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -12,19 +12,34 @@ class AdminUsersController < ApplicationController
   def list
     @admin_users = AdminUser.sorted
   end
-	def new
-	@Adminusers = AdminUser.new
-	end
+  
+  def new
+	@admin_user = AdminUser.new
+  end
 	
-	def create
-		
+  def create
+	@admin_user = AdminUser.new(params[:admin_user])
+	if @admin_user.save
+	  flash[:notice] = 'Admin user created.'
+	  redirect_to(:action => 'list')
+	else
+	  render("new")
 	end
+  end
 	
-	def edit
-	end
+  def edit
+	@admin_user = AdminUser.find(params[:id])
+  end
 	
-	def update
+  def update
+	@admin_user = AdminUser.find(params[:id])
+	if @admin_user.update_attributes(params[:admin_user])
+  	  flash[:notice] = 'Admin user updated.'
+	  redirect_to(:action => 'list')
+	else
+		render("edit")
 	end
+  end
 	
 	def delete
 	end
@@ -32,7 +47,5 @@ class AdminUsersController < ApplicationController
 	def destroy
 	end
 	
-	def list
-		
-	end
+
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -32,7 +32,7 @@ class AdminUser < ActiveRecord::Base
 	#new "sexy" validations
 	validates :first_name, :presence => true, :length => { :maximum => 25}
 	validates :last_name, :presence => true, :length => { :maximum => 50}
-	validates :username, :length => { :within => 8..25 }, :uniqueness => true
+	validates :username, :length => { :within => 8..25 }, :uniqueness => false
 	validates :email, :presence => true, :length => {:maximum => 100}, :format => EMAIL_REGEX, :confirmation => true
 	
 	#only on create, so other attributes of this user can be changed
